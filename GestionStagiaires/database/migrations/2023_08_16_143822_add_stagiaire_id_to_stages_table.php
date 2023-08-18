be727740a7d0be8e1cc3f43b05d375dd720a7a5b
nassimfatnassi1999 <fatnassihnifi.nassim@esprit.tn>
2023-08-18 02:05:01 +0100

    les migrations loses added

---
+++ GestionStagiaires/database/migrations/2023_08_16_143822_add_stagiaire_id_to_stages_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('stages', function (Blueprint $table) {
+            $table->unsignedBigInteger('stagiaire_id')->nullable();
+            $table->foreign('stagiaire_id')->references('id')->on('personnels')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('stages', function (Blueprint $table) {
+            //
+        });
+    }
+};
---
+++ GestionStagiaires/database/migrations/2023_08_18_001313_add_stage_id_to_personnels.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('personnels', function (Blueprint $table) {
+            $table->unsignedBigInteger('stage_id')->nullable();
+            $table->foreign('stage_id')->references('id')->on('stages');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('personnels', function (Blueprint $table) {
+            $table->dropForeign(['stage_id']);
+            $table->dropColumn('stage_id');
+        });
+    }
+};
